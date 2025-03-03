80efaa256cf7656e588250e8045029be93291b6a
martinolanza03 <lanzamartino3@gmail.com>
2025-03-03 16:32:03 +0100

    feat: fifth snacks

---
+++ 05_bonus/script.js
@@ -0,0 +1,30 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito. 
+Il saluto deve essere :
+buongiorno se è mattina (fino alle 13), 
+buon pomeriggio se è pomeriggio (fino alle 17) 
+e buonasera se è sera (oltre le 17)
+*/
+
+const name = 'Mario';
+
+
+// Dichiara la funzione qui.
+function greetDate (name) {
+    let data = new Date ();
+    let hours = data.getHours();
+
+    if (hours <= 13 && hours >= 5){
+        return `Buongiorno ${name}`;
+    } else if (hours > 13){
+        return `Buon pomeriggio ${name}`;
+    } else if (hours < 17){
+        return `Buonasera ${name}`;
+    }
+}
+
+// Invoca la funzione qui e stampa il risultato in console
+let greet = greetDate(name);
+console.log(greet);
+
+
+//Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
