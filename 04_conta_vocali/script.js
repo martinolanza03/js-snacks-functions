2cf9608ef6c1715374674b3574e3fcda4cb0f15c
martinolanza03 <lanzamartino3@gmail.com>
2025-03-03 16:14:30 +0100

    feat: bonus fourth snacks

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -23,3 +23,19 @@ console.log(vocalsCount(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
 
+const bonusVocalsCount = string => {
+    let vocals = ['a', 'e', 'i', 'o', 'u'];
+    let wordVocalsBonus = [];
+    for (let i = 0; i < string.length; i++) {
+        for (let index = 0; index < vocals.length; index++) {
+            if (string[i] === vocals[index]){
+                wordVocalsBonus.push(word[i]);
+            }
+        }
+    }
+    return wordVocalsBonus;
+}
+
+const anotherWord = 'paperino';
+const anotherVocals = bonusVocalsCount(anotherWord);
+console.log(anotherVocals);
